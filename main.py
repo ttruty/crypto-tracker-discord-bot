91e2b826a1ccacf872b51a7248a7db9f0ebddae3
ColdBio <64978825+ColdBio@users.noreply.github.com>
2021-09-24 17:28:24 +0100

    New v2.0 provides more data per coin/token
    This update provides a better user experience by providing more information in an easy to understand manner without being too overwhelming.

--- main.py
+++ main.py
@@ -13,8 +13,124 @@ link_data = cg.get_price(ids='chainlink', vs_currencies='gbp')
 avax_data = cg.get_price(ids='avalanche-2', vs_currencies='gbp')
 vet_data = cg.get_price(ids='vechain', vs_currencies='gbp')
 ada_data = cg.get_price(ids='cardano', vs_currencies='gbp')
-vet_data = cg.get_price(ids='vechain', vs_currencies='gbp')
 doge_data = cg.get_price(ids='dogecoin', vs_currencies='gbp')
+
+
+def get_more_data(coin):
+    more_data = cg.get_coins_markets(vs_currency="gbp", ids=f"{coin}")
+    coin_name = more_data[0]["name"]
+    coin_market_cap = more_data[0]["market_cap"]
+    coin_high_24h = more_data[0]["high_24h"]
+    coin_low_24h = more_data[0]["low_24h"]
+    coin_price_change_percent = more_data[0]["price_change_percentage_24h"]
+    coin_circulating_supply = more_data[0]["circulating_supply"]
+    coin_ath_price = more_data[0]["ath"]
+    coin_ath_change_percent = more_data[0]["ath_change_percentage"]
+    coin_image = more_data[0]["image"]
+    coin_current_price = more_data[0]["current_price"]
+    coin_atl = more_data[0]["atl"]
+
+    return coin_name, coin_market_cap, coin_high_24h, coin_low_24h, coin_price_change_percent, coin_circulating_supply, coin_ath_price, coin_ath_change_percent, coin_image, coin_current_price, coin_atl
+
+
+
+[btc_coin_name, 
+    btc_coin_market_cap, 
+    btc_coin_high_24h, 
+    btc_coin_low_24h, 
+    btc_coin_price_change_percent, 
+    btc_coin_circulating_supply, 
+    btc_coin_ath_price, 
+    btc_coin_ath_change_percent, 
+    btc_coin_image, 
+    btc_coin_current_price, 
+    btc_coin_atl] = get_more_data("bitcoin")
+
+[xrp_coin_name, 
+    xrp_coin_market_cap, 
+    xrp_coin_high_24h, 
+    xrp_coin_low_24h, 
+    xrp_coin_price_change_percent, 
+    xrp_coin_circulating_supply, 
+    xrp_coin_ath_price, 
+    xrp_coin_ath_change_percent, 
+    xrp_coin_image, 
+    xrp_coin_current_price, 
+    xrp_coin_atl] = get_more_data("ripple")
+
+[eth_coin_name, 
+    eth_coin_market_cap, 
+    eth_coin_high_24h, 
+    eth_coin_low_24h, 
+    eth_coin_price_change_percent, 
+    eth_coin_circulating_supply, 
+    eth_coin_ath_price, 
+    eth_coin_ath_change_percent, 
+    eth_coin_image, 
+    eth_coin_current_price, 
+    eth_coin_atl] = get_more_data("ethereum")
+
+[link_coin_name, 
+    link_coin_market_cap, 
+    link_coin_high_24h, 
+    link_coin_low_24h, 
+    link_coin_price_change_percent, 
+    link_coin_circulating_supply, 
+    link_coin_ath_price, 
+    link_coin_ath_change_percent, 
+    link_coin_image, 
+    link_coin_current_price, 
+    link_coin_atl] = get_more_data("chainlink")
+
+[ada_coin_name, 
+    ada_coin_market_cap, 
+    ada_coin_high_24h, 
+    ada_coin_low_24h, 
+    ada_coin_price_change_percent, 
+    ada_coin_circulating_supply, 
+    ada_coin_ath_price, 
+    ada_coin_ath_change_percent, 
+    ada_coin_image, 
+    ada_coin_current_price, 
+    ada_coin_atl] = get_more_data("cardano")
+
+
+[avax_coin_name, 
+    avax_coin_market_cap, 
+    avax_coin_high_24h, 
+    avax_coin_low_24h, 
+    avax_coin_price_change_percent, 
+    avax_coin_circulating_supply, 
+    avax_coin_ath_price, 
+    avax_coin_ath_change_percent, 
+    avax_coin_image, 
+    avax_coin_current_price, 
+    avax_coin_atl] = get_more_data("avalanche-2")
+
+[doge_coin_name, 
+    doge_coin_market_cap, 
+    doge_coin_high_24h, 
+    doge_coin_low_24h, 
+    doge_coin_price_change_percent, 
+    doge_coin_circulating_supply, 
+    doge_coin_ath_price, 
+    doge_coin_ath_change_percent, 
+    doge_coin_image, 
+    doge_coin_current_price, 
+    doge_coin_atl] = get_more_data("dogecoin")
+
+[vet_coin_name, 
+    vet_coin_market_cap, 
+    vet_coin_high_24h, 
+    vet_coin_low_24h, 
+    vet_coin_price_change_percent, 
+    vet_coin_circulating_supply, 
+    vet_coin_ath_price, 
+    vet_coin_ath_change_percent, 
+    vet_coin_image, 
+    vet_coin_current_price, 
+    vet_coin_atl] = get_more_data("vechain")
+
 
 trending_data = cg.get_search_trending()
 trending_tokens = []
@@ -69,6 +185,7 @@ doge_key = list(doge_data.values())[0]
 doge_price = list(doge_key.values())[0]
 
 
+
 client = discord.Client()
 
 
@@ -97,29 +214,228 @@ async def on_message(message):
     if message.content.startswith("$about"):
         await message.channel.send(f"Thank you for using this discord bot.\nTo view how I was made visit here: {repository}")
 
-    if message.content.startswith("$btc"):
-        await message.channel.send(f"£{btc_price}")
-    
-    if message.content.startswith("$eth"):
-        await message.channel.send(f"£{eth_price}")
-    
-    if message.content.startswith("$xrp"):
-        await message.channel.send(f"£{xrp_price}")
-    
-    if message.content.startswith("$link"):
-        await message.channel.send(f"£{link_price}")
-    
-    if message.content.startswith("$avax"):
-        await message.channel.send(f"£{avax_price}")
-    
-    if message.content.startswith("$vet"):
-        await message.channel.send(f"£{vet_price}")
-    
-    if message.content.startswith("$ada"):
-        await message.channel.send(f"£{ada_price}")
-        
-    if message.content.startswith("$doge"):
-        await message.channel.send(f"£{doge_price}")
-    
 
-client.run("{replace with your own discord bot token}")
+    if message.content.startswith('$btc'):
+        
+        #### Create the initial embed object ####
+        embed=discord.Embed(title=f"{btc_coin_name}")
+
+        # Add author, thumbnail, fields, and footer to the embed
+        embed.set_author(name=f"{client.user.name}", icon_url=client.user.avatar_url)
+
+        embed.set_thumbnail(url=f"{btc_coin_image}")
+
+        embed.add_field(name="Current Price 💵", value="£{:,}".format(btc_coin_current_price), inline=True)
+        embed.add_field(name="Circulating Supply 🪙", value="{:,}".format(btc_coin_circulating_supply), inline=True)
+        embed.add_field(name="Market Cap 🤑", value="£{:,}".format(btc_coin_market_cap), inline=True)
+
+        embed.add_field(name="24h-High ⬆️", value="£{:,}".format(btc_coin_high_24h), inline=True)
+        embed.add_field(name="24h-low ⬇️", value="£{:,}".format(btc_coin_low_24h), inline=True)
+        embed.add_field(name="Price Change 24h ⏰", value="{:,}%".format(round(btc_coin_price_change_percent, 2)), inline=True)
+
+        embed.add_field(name="All Time High 👑", value="£{:,}".format(btc_coin_ath_price), inline=True)
+        embed.add_field(name="ATH Percent Change 📊", value="{:,}%".format(btc_coin_ath_change_percent), inline=True)
+        embed.add_field(name="ATL 😢", value = "£{:,}".format(btc_coin_atl), inline=True)
+
+        embed.set_footer(text="Thank you for using Crypto Bot Price Checker 🙏")
+
+        await message.channel.send(embed=embed)
+
+    if message.content.startswith('$xrp'):
+        
+        #### Create the initial embed object ####
+        embed=discord.Embed(title=f"{xrp_coin_name}")
+
+        # Add author, thumbnail, fields, and footer to the embed
+        embed.set_author(name=f"{client.user.name}", icon_url=client.user.avatar_url)
+
+        embed.set_thumbnail(url=f"{xrp_coin_image}")
+
+        embed.add_field(name="Current Price 💵", value="£{:,}".format(xrp_coin_current_price), inline=True)
+        embed.add_field(name="Circulating Supply 🪙", value="{:,}".format(xrp_coin_circulating_supply), inline=True)
+        embed.add_field(name="Market Cap 🤑", value="£{:,}".format(xrp_coin_market_cap), inline=True)
+
+        embed.add_field(name="24h-High ⬆️", value="£{:,}".format(xrp_coin_high_24h), inline=True)
+        embed.add_field(name="24h-low ⬇️", value="£{:,}".format(xrp_coin_low_24h), inline=True)
+        embed.add_field(name="Price Change 24h ⏰", value="{:,}%".format(round(xrp_coin_price_change_percent, 2)), inline=True)
+
+        embed.add_field(name="All Time High 👑", value="£{:,}".format(xrp_coin_ath_price), inline=True)
+        embed.add_field(name="ATH Percent Change 📊", value="{:,}%".format(round(xrp_coin_ath_change_percent, 2)), inline=True)
+        embed.add_field(name="ATL 😢", value = "£{:,}".format(xrp_coin_atl), inline=True)
+
+
+        embed.set_footer(text="Thank you for using Crypto Bot Price Checker 🙏")
+
+
+        await message.channel.send(embed=embed)
+
+    if message.content.startswith('$eth'):
+        
+        #### Create the initial embed object #eth
+        embed=discord.Embed(title=f"{eth_coin_name}")
+
+        # Add author, thumbnail, fields, and footer to the embed
+        embed.set_author(name=f"{client.user.name}", icon_url=client.user.avatar_url)
+
+        embed.set_thumbnail(url=f"{eth_coin_image}")
+
+        embed.add_field(name="Current Price 💵", value="£{:,}".format(eth_coin_current_price), inline=True)
+        embed.add_field(name="Circulating Supply 🪙", value="{:,}".format(eth_coin_circulating_supply), inline=True)
+        embed.add_field(name="Market Cap 🤑", value="£{:,}".format(eth_coin_market_cap), inline=True)
+
+        embed.add_field(name="24h-High ⬆️", value="£{:,}".format(eth_coin_high_24h), inline=True)
+        embed.add_field(name="24h-low ⬇️", value="£{:,}".format(eth_coin_low_24h), inline=True)
+        embed.add_field(name="Price Change 24h ⏰", value="{:,}%".format(round(eth_coin_price_change_percent, 2)), inline=True)
+
+        embed.add_field(name="All Time High 👑", value="£{:,}".format(eth_coin_ath_price), inline=True)
+        embed.add_field(name="ATH Percent Change 📊", value="{:,}%".format(round(eth_coin_ath_change_percent, 2)), inline=True)
+        embed.add_field(name="ATL 😢", value = "£{:,}".format(eth_coin_atl), inline=True)
+
+
+        embed.set_footer(text="Thank you for using Crypto Bot Price Checker 🙏")
+
+
+        await message.channel.send(embed=embed)
+    
+    if message.content.startswith('$link'):
+        
+        #### Create the initial embed object #eth
+        embed=discord.Embed(title=f"{link_coin_name}")
+
+        # Add author, thumbnail, fields, and footer to the embed
+        embed.set_author(name=f"{client.user.name}", icon_url=client.user.avatar_url)
+
+        embed.set_thumbnail(url=f"{link_coin_image}")
+
+        embed.add_field(name="Current Price 💵", value="£{:,}".format(link_coin_current_price), inline=True)
+        embed.add_field(name="Circulating Supply 🪙", value="{:,}".format(link_coin_circulating_supply), inline=True)
+        embed.add_field(name="Market Cap 🤑", value="£{:,}".format(link_coin_market_cap), inline=True)
+
+        embed.add_field(name="24h-High ⬆️", value="£{:,}".format(link_coin_high_24h), inline=True)
+        embed.add_field(name="24h-low ⬇️", value="£{:,}".format(link_coin_low_24h), inline=True)
+        embed.add_field(name="Price Change 24h ⏰", value="{:,}%".format(round(link_coin_price_change_percent, 2)), inline=True)
+
+        embed.add_field(name="All Time High 👑", value="£{:,}".format(link_coin_ath_price), inline=True)
+        embed.add_field(name="ATH Percent Change 📊", value="{:,}%".format(round(link_coin_ath_change_percent, 2)), inline=True)
+        embed.add_field(name="ATL 😢", value = "£{:,}".format(link_coin_atl), inline=True)
+
+
+        embed.set_footer(text="Thank you for using Crypto Bot Price Checker 🙏")
+
+
+        await message.channel.send(embed=embed)
+    
+    if message.content.startswith('$ada'):
+        
+        #### Create the initial embed object #eth
+        embed=discord.Embed(title=f"{ada_coin_name}")
+
+        # Add author, thumbnail, fields, and footer to the embed
+        embed.set_author(name=f"{client.user.name}", icon_url=client.user.avatar_url)
+
+        embed.set_thumbnail(url=f"{ada_coin_image}")
+
+        embed.add_field(name="Current Price 💵", value="£{:,}".format(ada_coin_current_price), inline=True)
+        embed.add_field(name="Circulating Supply 🪙", value="{:,}".format(ada_coin_circulating_supply), inline=True)
+        embed.add_field(name="Market Cap 🤑", value="£{:,}".format(ada_coin_market_cap), inline=True)
+
+        embed.add_field(name="24h-High ⬆️", value="£{:,}".format(ada_coin_high_24h), inline=True)
+        embed.add_field(name="24h-low ⬇️", value="£{:,}".format(ada_coin_low_24h), inline=True)
+        embed.add_field(name="Price Change 24h ⏰", value="{:,}%".format(round(ada_coin_price_change_percent, 2)), inline=True)
+
+        embed.add_field(name="All Time High 👑", value="£{:,}".format(ada_coin_ath_price), inline=True)
+        embed.add_field(name="ATH Percent Change 📊", value="{:,}%".format(round(ada_coin_ath_change_percent, 2)), inline=True)
+        embed.add_field(name="ATL 😢", value = "£{:,}".format(ada_coin_atl), inline=True)
+
+
+        embed.set_footer(text="Thank you for using Crypto Bot Price Checker 🙏")
+
+
+        await message.channel.send(embed=embed)
+
+    if message.content.startswith('$avax'):
+        
+        #### Create the initial embed object #eth
+        embed=discord.Embed(title=f"{avax_coin_name}")
+
+        # Add author, thumbnail, fields, and footer to the embed
+        embed.set_author(name=f"{client.user.name}", icon_url=client.user.avatar_url)
+
+        embed.set_thumbnail(url=f"{avax_coin_image}")
+
+        embed.add_field(name="Current Price 💵", value="£{:,}".format(avax_coin_current_price), inline=True)
+        embed.add_field(name="Circulating Supply 🪙", value="{:,}".format(avax_coin_circulating_supply), inline=True)
+        embed.add_field(name="Market Cap 🤑", value="£{:,}".format(avax_coin_market_cap), inline=True)
+
+        embed.add_field(name="24h-High ⬆️", value="£{:,}".format(avax_coin_high_24h), inline=True)
+        embed.add_field(name="24h-low ⬇️", value="£{:,}".format(avax_coin_low_24h), inline=True)
+        embed.add_field(name="Price Change 24h ⏰", value="{:,}%".format(round(avax_coin_price_change_percent, 2)), inline=True)
+
+        embed.add_field(name="All Time High 👑", value="£{:,}".format(avax_coin_ath_price), inline=True)
+        embed.add_field(name="ATH Percent Change 📊", value="{:,}%".format(round(avax_coin_ath_change_percent, 2)), inline=True)
+        embed.add_field(name="ATL 😢", value = "£{:,}".format(avax_coin_atl), inline=True)
+
+
+        embed.set_footer(text="Thank you for using Crypto Bot Price Checker 🙏")
+
+
+        await message.channel.send(embed=embed)
+
+    if message.content.startswith('$doge'):
+        
+        #### Create the initial embed object #eth
+        embed=discord.Embed(title=f"{doge_coin_name}")
+
+        # Add author, thumbnail, fields, and footer to the embed
+        embed.set_author(name=f"{client.user.name}", icon_url=client.user.avatar_url)
+
+        embed.set_thumbnail(url=f"{doge_coin_image}")
+
+        embed.add_field(name="Current Price 💵", value="£{:,}".format(doge_coin_current_price), inline=True)
+        embed.add_field(name="Circulating Supply 🪙", value="{:,}".format(doge_coin_circulating_supply), inline=True)
+        embed.add_field(name="Market Cap 🤑", value="£{:,}".format(doge_coin_market_cap), inline=True)
+
+        embed.add_field(name="24h-High ⬆️", value="£{:,}".format(doge_coin_high_24h), inline=True)
+        embed.add_field(name="24h-low ⬇️", value="£{:,}".format(doge_coin_low_24h), inline=True)
+        embed.add_field(name="Price Change 24h ⏰", value="{:,}%".format(round(doge_coin_price_change_percent, 2)), inline=True)
+
+        embed.add_field(name="All Time High 👑", value="£{:,}".format(doge_coin_ath_price), inline=True)
+        embed.add_field(name="ATH Percent Change 📊", value="{:,}%".format(round(doge_coin_ath_change_percent, 2)), inline=True)
+        embed.add_field(name="ATL 😢", value = "£{:,}".format(doge_coin_atl), inline=True)
+
+
+        embed.set_footer(text="Thank you for using Crypto Bot Price Checker 🙏")
+
+
+        await message.channel.send(embed=embed)
+    
+    if message.content.startswith('$vet'):
+        
+        #### Create the initial embed object #eth
+        embed=discord.Embed(title=f"{vet_coin_name}")
+
+        # Add author, thumbnail, fields, and footer to the embed
+        embed.set_author(name=f"{client.user.name}", icon_url=client.user.avatar_url)
+
+        embed.set_thumbnail(url=f"{vet_coin_image}")
+
+        embed.add_field(name="Current Price 💵", value="£{:,}".format(vet_coin_current_price), inline=True)
+        embed.add_field(name="Circulating Supply 🪙", value="{:,}".format(vet_coin_circulating_supply), inline=True)
+        embed.add_field(name="Market Cap 🤑", value="£{:,}".format(vet_coin_market_cap), inline=True)
+
+        embed.add_field(name="24h-High ⬆️", value="£{:,}".format(vet_coin_high_24h), inline=True)
+        embed.add_field(name="24h-low ⬇️", value="£{:,}".format(vet_coin_low_24h), inline=True)
+        embed.add_field(name="Price Change 24h ⏰", value="{:,}%".format(round(vet_coin_price_change_percent, 2)), inline=True)
+
+        embed.add_field(name="All Time High 👑", value="£{:,}".format(vet_coin_ath_price), inline=True)
+        embed.add_field(name="ATH Percent Change 📊", value="{:,}%".format(round(vet_coin_ath_change_percent, 2)), inline=True)
+        embed.add_field(name="ATL 😢", value = "£{:,}".format(vet_coin_atl), inline=True)
+
+
+        embed.set_footer(text="Thank you for using Crypto Bot Price Checker 🙏")
+
+
+        await message.channel.send(embed=embed)
+
+
+client.run("{replace with your own token}")
